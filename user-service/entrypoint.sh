6391a75714ff67dccbb1e7ef2740680a2b920fed
aw520 <98403249+aw520@users.noreply.github.com>
2026-02-27 18:04:25 -0500

    revert entrypoint.sh

--- user-service/entrypoint.sh
+++ user-service/entrypoint.sh
@@ -5,14 +5,10 @@ mkdir -p /run/secrets
 #Let AWS inject these as strings into the container
 if [ ! -z "$JWT_PRIVATE_KEY_CONTENT" ]; then
   echo "$JWT_PRIVATE_KEY_CONTENT" > /run/secrets/jwt-private.key
-else
-  echo '' >  /run/secrets/jwt-private.key
 fi
 
 if [ ! -z "$JWT_PUBLIC_KEY_CONTENT" ]; then
   echo "$JWT_PUBLIC_KEY_CONTENT" > /run/secrets/jwt-public.key
-else
-  echo '' > /run/secrets/jwt-public.key
 fi
 
 #hand over control to the Java application
